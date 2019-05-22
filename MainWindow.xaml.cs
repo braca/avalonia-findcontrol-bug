d0101ba95eb122c25ba3757a5b7b080750e69aa4
Raul Bravo <br4ca@hotmail.com>
2019-05-22 17:20:56 +0100

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaFindControl
+{
+    public class MainWindow : Window
+    {
+        private readonly SampleUserControl sampleUserControl;
+        private readonly TextBlock textBlock;
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            this.textBlock = this.FindControl<TextBlock>("textBlock");
+            this.sampleUserControl = this.FindControl<SampleUserControl>("sampleUserControl");
+
+            if (textBlock == null)
+            {
+                throw new Exception("Textblock was not found");
+            }
+
+            if (sampleUserControl == null)
+            {
+                throw new Exception("SampleUserControl was not found");
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ SampleUserControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaFindControl
+{
+    public class SampleUserControl : UserControl
+    {
+        public SampleUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
